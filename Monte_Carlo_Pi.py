786ff0bd4afc75188d4e2cc2088c72bbbf4df013
WillKwon <will.dw.kwon@gmail.com>
2015-02-11 23:27:30 +0900

    1

---
+++ Monte_Carlo_Pi.py
@@ -0,0 +1,50 @@
+'''
+Approximation of pi using Monte Carlo simulation
+
+'''
+import random
+import math
+random.seed(201502)
+
+
+## PROCEDURES
+
+# Generate n points in (0,1)
+def unif_point_generator(n):
+    i = 0; Data = []
+    while i < n:
+        Data.append(round(random.uniform(0,1), 2))
+        i+=1
+    return Data
+
+# Scale points from (0,1) to (-b,b)
+def scale_to_coordinate(a, X, b):
+    X = [round((x*a + b),2) for x in X]
+    return X
+
+# Count how many points fall in the circle of radius r
+def in_or_out_circle(X, Y, r):
+    i = 0; j = 0; count = 0
+    length = len(X)
+    Z = []
+    while i < length:
+        Z.append(math.sqrt(X[i]**2+Y[i]**2))
+        i+=1
+    while j < length:
+        if Z[j] < 1:
+            count += 1
+        j+=1
+    return count, Z
+####
+    
+    
+# Generate X and Y coordinates ranging (-1,1)
+X = scale_to_coordinate(2, unif_point_generator(1000), -1)
+Y = scale_to_coordinate(2, unif_point_generator(1000), -1)
+# Count how many number of the points are within the circle of radius r
+num, Z = in_or_out_circle(X, Y, 1)
+# Get the proportion and solve for pi
+Estimated_Pi = ((num*4)/len(Z))
+
+print ("The Monte Carlo Estimation for Pi is: ", end = "")
+print (Estimated_Pi)
